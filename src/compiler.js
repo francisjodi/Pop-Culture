4dc2aafeefc6bd4c7ce9ea52abba09fab2b5c2f5
sumaiyahlee <53411585+sumaiyahlee@users.noreply.github.com>
2022-02-23 10:28:55 -0800

    updating files & adding to compiler

---
+++ src/compiler.js
@@ -0,0 +1,9 @@
+import ast from "./ast.js"
+
+export default function compile(source, outputType) {
+    if (![ast].includes(outputType)) {
+        throw new Error("Unknown output type")
+    }
+    const program = ast(source)
+    if (outputType === "ast") return program
+}
--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,21 +1,29 @@
-import assert from "assert"
-import { add, times } from "../src/popCulture.js"
+import assert from "assert/strict"
+import util from "util"
+import compile from "../src/compiler.js"
+
+const sampleProgram = "print 0"
 
 describe("The Compiler", () => {
-  describe("has an add function()", () => {
-    it("should return 4 when adding 2 and 2 ", () => {
-      assert.deepEqual(add(2,2),4)
-    })
+  it("throws when the output type is unknown", (done) => {
+    assert.throws(() => compile(sampleProgram, ""), /Unknown output type/)
+    done()
   })
 
-describe("has a times function", () => {
-    it("should return 4 when multiplying 2 and 2 ", () => {
-      assert.deepEqual(times(2, 2), 4)
-    })
-    it("returns 0 when multipling by 0 ", () => {
-      assert.deepEqual(times(2,0),0)
-      assert.deepEqual(times(0,98765456789),0)
-      assert.deepEqual(times(0,0),0)
-    })
+  it("accepts the ast option", (done) => {
+    const compiled = compile(sampleProgram, "ast")
+    assert(util.format(compiled).startsWith("   1 | Program"))
+    done()
   })
+
+  // describe("has a times function", () => {
+  //     it("should return 4 when multiplying 2 and 2 ", () => {
+  //       assert.deepEqual(times(2, 2), 4)
+  //     })
+  //     it("returns 0 when multipling by 0 ", () => {
+  //       assert.deepEqual(times(2,0),0)
+  //       assert.deepEqual(times(0,98765456789),0)
+  //       assert.deepEqual(times(0,0),0)
+  //     })
+  //   })
 })
