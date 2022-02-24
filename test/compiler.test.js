5b3084a3578be4f197c7083156216fa40a664518
sumaiyahlee <53411585+sumaiyahlee@users.noreply.github.com>
2022-02-23 17:28:07 -0800

    adding arrays, functions with statement bodies, loops and if statements to the grammar

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -16,14 +16,4 @@ describe("The Compiler", () => {
     done()
   })
 
-  // describe("has a times function", () => {
-  //     it("should return 4 when multiplying 2 and 2 ", () => {
-  //       assert.deepEqual(times(2, 2), 4)
-  //     })
-  //     it("returns 0 when multipling by 0 ", () => {
-  //       assert.deepEqual(times(2,0),0)
-  //       assert.deepEqual(times(0,98765456789),0)
-  //       assert.deepEqual(times(0,0),0)
-  //     })
-  //   })
 })
