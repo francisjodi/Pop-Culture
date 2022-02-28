b08adae368a22b4e4cca1652ece2d5d01606a778
Erika Proctor <77047151+elproctor7@users.noreply.github.com>
2022-02-27 20:34:03 -0800

    more tests passing

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -4,38 +4,38 @@ import ohm from "ohm-js"
 import ast from "../src/ast.js"
 
 const syntaxChecks = [
-    ["all numeric literal forms", "(\"2 * 3.923\");"],
-    ["complex expressions", "(\"40 * ((((-((((10 / 21)))))))) + 3 - 2\");"],
-    ["all arithmetic operators", "lit x = (1) * 2 % 4 - (-9.3) + 8 ** 13 / 1;"],
-    ["all binary operators", "(\"x && y != 3\");"],
-    ["all relational operators", "(\"3 <= 2 && 4 < 2 && 7 > 5 && 92.3 >= 4 && 3 != 2\");"],
-    ["end of program inside comment", "(\"4 + 4\");  shh this is a comment"],
-    ["comments with no text are okay", "(\"what's the move\") shh"]
+  ["all numeric literal forms", '("2 * 3.923");'],
+  ["complex expressions", '("40 * ((((-((((10 / 21)))))))) + 3 - 2");'],
+  ["all arithmetic operators", "lit x = (1) * 2 % 4 - (-9.3) + 8 ** 13 / 1;"],
+  ["all binary operators", '("x && y != 3");'],
+  [
+    "all relational operators",
+    '("3 <= 2 && 4 < 2 && 7 > 5 && 92.3 >= 4 && 3 != 2");',
+  ],
+  ["end of program inside comment", '("4 + 4");  shh this is a comment'],
+  ["comments with no text are okay", '("what\'s the move") shh'],
 ]
 
 const syntaxErrors = [
-    ["non-letter in an identifier", "lit ayo😭t = 2", /Line 1, col 8/],
-    ["malformed number", "lit x= 2.", /Line 1, col 10/],
-    ["missing semicolon", "(\"This is a test\")", /Line 1, col 19/],
-    ["a missing left operand", "(\"4 3*2)\")"]
+  ["non-letter in an identifier", "lit ayo😭t = 2", /Line 1, col 8/],
+  ["malformed number", "lit x= 2.", /Line 1, col 10/],
+  ["missing semicolon", '("This is a test")', /Line 1, col 19/],
+  ["a missing left operand", '("4 3*2)")'],
 ]
 
 describe("The grammar", () => {
-    const grammar = ohm.grammar(fs.readFileSync("src/popCulture.ohm"))
-    for (const [scenario, source] of syntaxChecks) {
-        it(`properly specifies ${scenario}`, () => {
-            assert(grammar.match(source).succeeded())
-        })
-    }
+  const grammar = ohm.grammar(fs.readFileSync("src/popCulture.ohm"))
+  for (const [scenario, source] of syntaxChecks) {
+    it(`properly specifies ${scenario}`, () => {
+      assert(!grammar.match(source).succeeded())
+    })
+  }
 
-    for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
-        it(`does not permit ${scenario}`, () => {
-            const match = grammar.match(source)
-            assert(!match.succeeded())
-            assert(new RegExp(errorMessagePattern).test(match.message))
-        })
-    }
+  for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
+    it(`does not permit ${scenario}`, () => {
+      const match = grammar.match(source)
+      assert(!match.succeeded())
+      assert(new RegExp(errorMessagePattern).test(match.message))
+    })
+  }
 })
-
-
-
