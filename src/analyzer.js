bfc19a2ab15996e2f8c1e5ba22776c9598e58585
nelson6400 <nelson6400@gmail.com>
2022-04-12 11:37:02 -0700

    worked on analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,18 +1,32 @@
+import { Type } from "js-yaml"
 import {
-    Assignment,
-    Call,
-    Variable,
-    Function,
-    ArrayExpression,
-    SubscriptExpression,
-    Token,
-    error
-} from "./core.js"
+Program,
+Assignment,
+Call,
+Variable,
+Function,
+WhileStatement,
+PrintStatement,
+BinaryExpression,
+UnaryExpression,
+VariableDeclaration,
+FunctionDeclaration,
+BreakStatement,
+ReturnStatement,
+IfStatement,
+ShortIfStatement,
+ForStatement, 
+EmptyArray,
+ArrayExpression, 
+SubscriptExpression, 
+Token, 
+error
+} from core.js
 
 Object.assign(ArrayExpression.prototype, {
     isEquivalentTo(target) {
         return (
-            target.constructor === ArrayExpression && this.baseType.isEquivalentTo(target.baseType)
+            target.constructor === ArrayExpression && this.baseType.isEquivalentTo(target.baseTypegi)
         )
     },
     isAssignableTo(target) {
