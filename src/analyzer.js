1bc93702a5ea30ba9f95fdb3a1b7fe8d9f3a046a
nelson6400 <nelson6400@gmail.com>
2022-04-12 12:26:50 -0700

    More progress on analyzer.js

--- src/analyzer.js
+++ src/analyzer.js
@@ -23,13 +23,60 @@ Token,
 error
 } from core.js
 
+Object.assign(Type.prototype, {
+    isEquivalentTo(target) {
+        return this == target
+    },
+    isAssignableTo(target) {
+        return this.isEquivalentTo(target)
+    },
+})
+
 Object.assign(ArrayExpression.prototype, {
     isEquivalentTo(target) {
         return (
-            target.constructor === ArrayExpression && this.baseType.isEquivalentTo(target.baseTypegi)
+            target.constructor === ArrayExpression && this.baseType.isEquivalentTo(target.baseType)
         )
     },
     isAssignableTo(target) {
         return this.isEquivalentTo(target)
     }
 })
+
+Object.assign(SubscriptExpression.prototype, {
+    isEquivalentTo(target) {
+        return (
+            target.constructor == SubscriptExpression && 
+            this.array.baseType.isEquivalentTo(target.array.baseType)
+        )
+    },
+    isAssignableTo(target) {
+        return this.isEquivalentTo(target)
+    }
+})
+
+Object.assign(Function.prototype, {
+    isEquivalentTo(target) {
+        return (
+            target.constructor === Function &&
+            this.paramCount === target.paramCount &&
+            this.readOnly === target.readOnly
+        )
+    },
+    isAssignableTo(target) {
+        return (
+            target.constructor === Function &&
+            this.paramCount === target.paramCount &&
+            this.readOnly === target.readOnly
+        )
+    }
+})
+
+export default function analyze(node) {
+    const initialContext = new Context({})
+    for (const [name, type] of Object.entries(stdlib.contents)) {
+      initialContext.add(name, type)
+    }
+    initialContext.analyze(node)
+    return node
+}
