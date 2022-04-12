bf7347d1945d7749af6fb3b6c441451d4cd27577
Jodi-Ann Francis <53411733+francisjodi@users.noreply.github.com>
2022-04-12 00:42:39 -0700

    pages update but needs to be edited

---
+++ src/analyzer.js
@@ -0,0 +1,27 @@
+import { Type } from "js-yaml"
+import {
+Program,
+Assignment,
+Call,
+Variable,
+Function,
+WhileStatement,
+PrintStatement,
+BinaryExpression,
+UnaryExpression,
+VariableDeclaration,
+FunctionDeclaration,
+BreakStatement,
+ReturnStatement,
+IfStatement,
+ShortIfStatement,
+ForStatement, 
+EmptyArray,
+ArrayExpression, 
+SubscriptExpression, 
+Token, 
+error
+} from core.js
+
+
+
