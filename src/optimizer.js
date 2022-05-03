c270932e13ec2cc31877bd796b28f7f45e1825fb
Jodi-Ann Francis <53411733+francisjodi@users.noreply.github.com>
2022-05-02 20:22:41 -0700

    starting optimizer

---
+++ src/optimizer.js
@@ -0,0 +1,4 @@
+import * as core from "./core.js"
+
+
+
---
+++ src/stdlib.js
@@ -0,0 +1,9 @@
+import { Type } from "./core.js"
+
+export const types = Object.freeze({
+  int: Type.INT,
+  float: Type.DOUBLE,
+  bool: Type.BOOLEAN,
+  String: Type.STRING,
+  any: Type.ANY,
+})
