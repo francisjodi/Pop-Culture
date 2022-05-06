fcfab1e473114c7b0a380b7139a31817c29617f1
Izabela Aranda-Dooley <izabela@arandadooley.com>
2022-05-06 16:44:19 -0700

    adding grammar tests

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -14,14 +14,32 @@ const syntaxChecks = [
   ],
   ["end of program inside comment", "lit x=1;  shh this is a comment"],
   ["comments with no text are okay", 'lit x="what\'s the move"; shh'],
-  ["if statement", "as if x==1 { sayItWithYourChest forRealz || urDone; }"],
+  ["short if statement", "as if x==1 { sayItWithYourChest forRealz || urDone; }"],
+  ["long if statement", 'as if x==1 { sayItWithYourChest ("first"); } ugh { sayItWithYourChest ("second"); }'],
+  ["longest if statement", 'as if x==1 { sayItWithYourChest("here");} ugh as if x==2 {sayItWithYourChest("there");} ugh {sayItWithYourChest("anywhere");}'],
+  ["while loop with one statement block", 'sayLess x==1 { sayItWithYourChest("hey"); }'],
+  ["while loop with empty block", "sayLess x==1 {}"],
+  ["boolean literals", "lit x = forRealz || urDone;"],
+  ["empty string", 'lit x = "";'],
+  // ["for loop", "keepItUp ( lit i = 0; i < 5; i++ ) { i += 1; }"],
+  // ["", ""],
 ]
 
 const syntaxErrors = [
-  ["non-letter in an identifier", "lit ayo😭t = 2", /Line 1, col 8/],
+  ["non-letter in an identifier", "lit ayo😭t = 2;", /Line 1, col 8/],
   ["an expression starting with a *", "x = * 71;", /Line 1, col 5/],
   ["missing semicolon", "lit x = 1", /Line 1, col 10/],
-  ["a missing left operand", "lit x = 1 *", /Line 1, col 12/],
+  ["a missing left operand", "lit x = 1 *;", /Line 1, col 12/],
+  ["a missing right parenthesis", "lit x = (1 + 2;", /Line 1, col 15/],
+  ["a missing left parenthesis", "lit x = 1 + 2);", /Line 1, col 14/],
+  ["mixing ands and ors", "print(1 && 2 || 3);", /Line 1, col 15/],
+  ["mixing ors and ands", "print(1 || 2 && 3);", /Line 1, col 15/],
+  ["associating relational operators", "print(1 < 2 < 3);", /Line 1, col 13/],
+  ["bad array literal", "lit x = [1,2,];", /Line 1, col 14/],
+  ["reserved word as an identifier", "lit stickIt = 1;", /Line 1, col 5/],
+  // ["missing identifier between two operators", "lit x = 7 + - 4;", /Line 1, col 12/],
+        //this is currently allowed
+  ["unbalanced brackets", "lit x = [1,2,3;", /Line 1, col 15/],
 ]
 
 describe("The grammar", () => {
