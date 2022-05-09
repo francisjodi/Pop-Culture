97ef6c553b3a8f37c90b5c9b6c083fc5e8daf1b7
Erika Proctor <77047151+elproctor7@users.noreply.github.com>
2022-05-08 20:30:36 -0700

    fixing errors in optimizer

--- test/optimizer.test.js
+++ test/optimizer.test.js
@@ -8,8 +8,8 @@ const x = new core.Variable("x", false)
 const return1p1 = new core.ReturnStatement(new core.BinaryExpression("+", 1, 1))
 const return2 = new core.ReturnStatement(2)
 const returnX = new core.ReturnStatement(x)
-// const onePlusTwo = new core.BinaryExpression("+", 1, 2)
-// const identity = Object.assign(new core.Function("id"), { body: returnX })
+const onePlusTwo = new core.BinaryExpression("+", 1, 2)
+const identity = Object.assign(new core.Function("id"), { body: returnX })
 const intFun = (body) => new core.FunctionDeclaration("f", [], "int", body)
 // const callIdentity = (args) => new core.Call(identity, args)
 // const or = (...d) => d.reduce((x, y) => new core.BinaryExpression("||", x, y))
@@ -20,15 +20,15 @@ const times = (x, y) => new core.BinaryExpression("*", x, y)
 const neg = (x) => new core.UnaryExpression("-", x)
 // const array = (...elements) => new core.ArrayExpression(elements)
 // const emptyArray = new core.EmptyArray(core.Type.INT)
-// const sub = (a, e) => new core.SubscriptExpression(a, e)
-// const unwrapElse = (o, e) => new core.BinaryExpression("??", o, e)
+const sub = (a, e) => new core.SubscriptExpression(a, e)
+const unwrapElse = (o, e) => new core.BinaryExpression("??", o, e)
 // const conditional = (x, y, z) => new core.Conditional(x, y, z)
 // const emptyOptional = new core.EmptyOptional(core.Type.INT)
-// const some = (x) => new core.UnaryExpression("some", x)
+const some = (x) => new core.UnaryExpression("some", x)
 
 const tests = [
   ["folds +", new core.BinaryExpression("+", 5, 8), 13],
-  ["folds -", new core.BinaryExpression("-", 5n, 8n), -3n],
+  ["folds -", new core.BinaryExpression("-", 5, 8), -3],
   ["folds *", new core.BinaryExpression("*", 5, 8), 40],
   ["folds /", new core.BinaryExpression("/", 5, 8), 0.625],
   ["folds **", new core.BinaryExpression("**", 5, 8), 390625],
@@ -82,7 +82,7 @@ const tests = [
   //   ["optimizes away nil", unwrapElse(emptyOptional, 3), 3],
   //   ["optimizes left conditional true", conditional(true, 55, 89), 55],
   //   ["optimizes left conditional false", conditional(false, 55, 89), 89],
-  //  ["optimizes in functions", intFun(return1p1), intFun(return2)],
+  //   ["optimizes in functions", intFun(return1p1), intFun(return2)],
   //   ["optimizes in subscripts", sub(x, onePlusTwo), sub(x, 3)],
   //   ["optimizes in array literals", array(0, onePlusTwo, 9), array(0, 3, 9)],
   //   ["optimizes in arguments", callIdentity([times(3, 5)]), callIdentity([15])],
@@ -108,7 +108,7 @@ const tests = [
   //       new core.WhileStatement(true, [new core.BreakStatement()]),
   //       new core.RepeatStatement(5, [new core.ReturnStatement(1)]),
   //       conditional(x, 1, 2),
-  //       unwrapElse(some(x), 7),
+  //   unwrapElse(some(x), 7),
   //       new core.IfStatement(x, [], []),
   //       new core.ShortIfStatement(x, []),
   //       new core.ForRangeStatement(x, 2, "..<", 5, []),
