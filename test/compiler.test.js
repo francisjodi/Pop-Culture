9e53d4809fd4dbcfcfd8ddcf2a797bb59b3bb8b9
Erika Proctor <77047151+elproctor7@users.noreply.github.com>
2022-05-08 20:49:31 -0700

    fixing compiler error

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -2,14 +2,14 @@ import assert from "assert/strict"
 import util from "util"
 import compile from "../src/compiler.js"
 
-const sampleProgram = "sayItWithYourChest(0);"
+const sampleProgram = "sayItWithYourChest(urDone);"
 
 describe("The compiler", () => {
-  it("throws when the output type is unknown", done => {
+  it("throws when the output type is unknown", (done) => {
     assert.throws(() => compile(sampleProgram, "blah"), /Unknown output type/)
     done()
   })
-  it("accepts the ast option", done => {
+  it("accepts the ast option", (done) => {
     const compiled = compile(sampleProgram, "ast")
     assert(util.format(compiled).startsWith("   1 | Program"))
     done()
